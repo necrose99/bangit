1de8a0c4e50aaa51697e6128c0bc9b577dbf4755
Michael L. <mike@michaellawrenceit.com>
2026-04-03 19:08:40 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,6 @@ setup(
     name="bangit",
     version="0.1.0",
     py_modules=["bangit"],
-    install_requires=[],
     entry_points={
         "console_scripts": [
             "bangit=bangit:main",
